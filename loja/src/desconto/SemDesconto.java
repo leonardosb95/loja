270fcaa3959f95e5ff43528a92a115e5b6185965
Leonardo Bispo <leonardo.bispo@neobpo.com.br>
2021-09-09 14:22:13 -0300

    VIDEO 03 - Aplicando o pattern Chain of Responsibility

---
+++ loja/src/desconto/SemDesconto.java
@@ -0,0 +1,17 @@
+package desconto;
+
+import orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class SemDesconto extends Desconto{
+
+    public SemDesconto(){
+        super(null);
+    }
+
+    @Override
+    public BigDecimal calcular(Orcamento orcamento) {
+        return BigDecimal.ZERO;
+    }
+}
--- loja/src/imposto/CalculadoraDeDescontos.java
+++ loja/src/imposto/CalculadoraDeDescontos.java
@@ -1,5 +1,9 @@
 package imposto;
 
+import desconto.Desconto;
+import desconto.DescontoParaOrcamentoComMaisDeCincoItens;
+import desconto.DescontoParaOrcamentoComValorMaiorQueQuinhentos;
+import desconto.SemDesconto;
 import orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -7,12 +11,9 @@ import java.math.BigDecimal;
 public class CalculadoraDeDescontos {
 
     public BigDecimal calcular(Orcamento orcamento){
-        if(orcamento.getQuantidadeItens()>5){
-            return orcamento.getValor().multiply(new BigDecimal("0.1"));
-        }
-        if(orcamento.getValor().compareTo(new BigDecimal("500"))>0){
-            return orcamento.getValor().multiply(new BigDecimal("0.1"));
-        }
-        return BigDecimal.ZERO;
+        Desconto desconto = new DescontoParaOrcamentoComMaisDeCincoItens(
+               new DescontoParaOrcamentoComValorMaiorQueQuinhentos(
+                    new SemDesconto()));
+        return desconto.calcular(orcamento);
     }
 }
