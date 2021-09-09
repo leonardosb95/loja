f0ce4bc15461d7ef113d9f110e2821bc2c5a9017
Leonardo Bispo <leonardo.bispo@neobpo.com.br>
2021-09-09 12:04:48 -0300

    VIDEO 03 - Aplicando o pattern Strategy

--- loja/src/teste/TestesImpostos.java
+++ loja/src/teste/TestesImpostos.java
@@ -1,7 +1,7 @@
 package teste;
 
 import imposto.CalculadoraDeImpostos;
-import imposto.TipoImposto;
+import imposto.ICMS;
 import orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -10,6 +10,6 @@ public class TestesImpostos {
     public static void main(String[] args) {
         Orcamento orcamento =new Orcamento(new BigDecimal("100"));
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-        System.out.println(calculadora.calcular(orcamento, TipoImposto.ICMS));
+        System.out.println(calculadora.calcular(orcamento,new ICMS()));
     }
 }
